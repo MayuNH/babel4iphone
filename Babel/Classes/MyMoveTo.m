4eb21ec82ea7272d316f066f0420926955990e96
Giovanni Amati <amatig@gmail.com>
2010-02-17 11:21:57 +0100

    licence

--- Babel/Classes/MyMoveTo.m
+++ Babel/Classes/MyMoveTo.m
@@ -1,10 +1,20 @@
-//
-//  MyMoveTo.m
-//  Genoma
-//
-//  Created by Giovanni Amati on 13/10/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+// This file is part of babel4iphone.
+
+// Copyright (C) 2009 Giovanni Amati <amatig@gmail.com>
+
+// babel4iphone is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// babel4iphone is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with babel4iphone.  If not, see <http://www.gnu.org/licenses/>.
+
 
 /*#import "MyMoveTo.h"
 
--- Net/reactor.py
+++ Net/reactor.py
@@ -1,6 +1,22 @@
-import time
+# This file is part of babel4iphone.
 
-# Class for callLater, step() need called in main loop
+# Copyright (C) 2009 Giovanni Amati <amatig@gmail.com>
+
+# babel4iphone is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# babel4iphone is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with babel4iphone.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import time
 
 class Reactor(object):
     
